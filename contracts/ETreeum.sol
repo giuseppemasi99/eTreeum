8fb08f07ec195e877623027b210cf22d120ad43a
Giuseppe <masi.1962771@studenti.uniroma1.it>
2022-02-03 10:22:15 +0100

    removed bootstrap, added visibility specifier to the methods

--- contracts/ETreeum.sol
+++ contracts/ETreeum.sol
@@ -9,12 +9,12 @@ contract ETreeum {
     // organization who plants real-world plants
     address payable private planter;
 
-    enum SpeciesNames = {specie1, specie2, ...};
-    enum StagesNames = {Seed, Ok, Good, ...};
+    enum SpeciesNames{SPECIE1, SPECIE2}
+    enum StagesNames{Seed, Ok, Good}
 
-    constant int MAX_RARITY = 2; // maximum number of levels of rarity [0 -> normal, 1-> low risk, 2 -> high risk]
-    constant int SEED_PRICE = 150; // price of a seed
-    constant int PERCENTAGE_FOR_REAL_PLANT = 5; // percentage of ETH used for plant real tree
+    int constant MAX_RARITY = 2; // maximum number of levels of rarity [0 -> normal, 1-> low risk, 2 -> high risk]
+    int constant SEED_PRICE = 150; // price of a seed
+    int constant PERCENTAGE_FOR_REAL_PLANT = 5; // percentage of ETH used for plant real tree
 
     struct Specie {
         int rarity; // level of rarity
@@ -33,16 +33,16 @@ contract ETreeum {
         int value; // value of the tree as linear combination of stage and rarity
     }
 
-    private Specie[] species;
+    Specie[] private species;
 
     // user_address -> user_nickname
-    private mapping(address => string) address2nickname;
+    mapping(address => string) private address2nickname;
 
     // user_address -> [Tree1, Tree2, ...]
-    private mapping(address => Tree[]) user2trees;
+    mapping(address => Tree[]) private user2trees;
 
     // tree_id -> tree_price
-    private mapping(int => int) shop;
+    mapping(int => int) private shop;
 
     constructor() {
 
@@ -55,56 +55,56 @@ contract ETreeum {
     // create a new tree and store it in the user2trees mapping
     // input: nickname
     // output: Tree
-    function plantSeed(){}
+    function plantSeed() public {}
 
     // change nickname of the tree
     // input: new_nickname
     // output: bool (success or failure)
-    function renameTree(){}
+    function renameTree() public {}
 
     // check if the tree is going to grow or not (and eventually update the tree status)
     // input:
     // output: (new)status
-    function growTree(){}
+    function growTree() public {}
 
     // give water to the tree
     // input: tree_id, amount_of_millimeters
     // output: (new)status
-    function giveWater(){}
+    function giveWater() public {}
 
     // give sun to the tree
     // input: tree_id, hours_of_sun
     // output: (new)status
-    function giveSun(){}
+    function giveSun() public {}
 
     // adding a new type of Tree
     // only the minter is able to run this method
     // input: Specie
     // output: bool (success or failure)
     // events: emit event when a new specie is added by the minter
-    function addSpecie(){}
+    function addSpecie() public {}
 
     // create a new tree and store it in the user2trees mapping
     // this method requires some weis and then call the method plantSeed()
     // input: nickname
     // output: Tree
-    function buySeed(){}
+    function buySeed() public {}
 
     // computing the score of the user as sum of the owned trees
     // input: user_address
     // output: user_score
-    function computeUserScore(){}
+    function computeUserScore() public {}
 
     // compute the ranking and store it in the ranking field, by iterating on all the users
     // input:
     // output:
     // events: emit event when the ranking is going to change
-    function computeRanking(){}
+    function computeRanking() public {}
 
     // get the ranking
     // input:
     // output: list of three users
-    function getRanking(){}
+    function getRanking() public {}
 
     // the new owner but the tree from the old owner
     //      (update user2trees, send the wei to the old owner)
@@ -113,18 +113,18 @@ contract ETreeum {
     // input: tree_id
     // output: bool (success or failure)
     // events: emit event when the tree is sold
-    function buyTree(){}
+    function buyTree() public {}
 
     // store the tree in the shop field at the price given by user (check that the price is in the range of the value of the tree)
     // input: tree_id, user_given_price
     // output: bool (success or failure)
     // events: emit event when the tree is inserted in the shop
-    function sellTree(){}
+    function sellTree() public {}
 
     // destruct everything
     // check that the sender is the minter
     // input:
     // output:
-    function selfDestruction(){}
+    function selfDestruction() public {}
 
 }
